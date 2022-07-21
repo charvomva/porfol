c0fbf1f4fd2801cad7c3c4e2055c374b4a9a6a7d
charvomva <mk28ar@gmail.com>
2022-07-21 16:36:23 +0900

    흐흫

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ new fullpage('#fullpage', {
     autoScrolling:true,
     scrollHorizontally: true,
     scrollBar:true,
-    sectionsColor: ['#fff', '#EBE7E4', '#FFC19E', '#BCE55C','#5CD1E5'],
+    sectionsColor: ['#fff', '#000000', '#FFC19E', '#BCE55C','#5CD1E5'],
     navigation: true,
     navigationPosition: 'right',
     navigationTooltips: ['GAH소개','제다큐어','연구개발','본사위치','연구소위치'], 
